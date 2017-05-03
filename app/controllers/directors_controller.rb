2bbf3968fcd6f58462bf2052e2c830b085197323
Sushil Kumar <sushilkumar@chicagobooth.edu>
2017-05-03 00:04:23 -0500

    1st trial

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,13 +1,13 @@
 class DirectorsController < ApplicationController
   def index
-    directors = Director.all
+    @directors = Director.all
   end
 
   def show
     @director = Director.find(params[:id])
   end
 
-  def new
+  def new_form
   end
 
   def create_row
